cfb0cc46644167da9f7172030750e7da46288e60
James Bradley <hintonbradley@gmail.com>
2018-03-16 11:12:15 -0700

    initial setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAnaFHCGmbxIlPrUxd2RjsTjrv7rUd-CNQ",
+    authDomain: "bradley-design.firebaseapp.com",
+    databaseURL: "https://bradley-design.firebaseio.com",
+    projectId: "bradley-design",
+    storageBucket: "",
+    messagingSenderId: "730006213203"
+  }
+};
