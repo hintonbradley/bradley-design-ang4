8e89486748216a29007740c2616c011271101b46
James Bradley <hintonbradley@gmail.com>
2018-03-16 13:22:45 -0700

    added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,35 @@ import { NgModule } from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import {RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    ProjectComponent,
+    ProjectsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
-
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path: '', component: HomepageComponent},
+      {path: 'projects/:project-name', component: ProjectsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
